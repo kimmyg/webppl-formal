48b0d1f1022c42c6326dea61272f7fef3eb52f22
Kimball Germane <krgermane@gmail.com>
2015-03-05 22:51:53 -0700

    First pass at influence.

--- full/analyze.rkt
+++ full/analyze.rkt
@@ -1,12 +1,23 @@
 #lang racket/base
-(require racket/match
+(require #;(for-syntax racket/base)
+         racket/match #;(rename-in racket/match [match rkt:match])
 	 racket/set
 	 "parse.rkt")
 
+
+#;
+(define-syntax (match stx)
+  (syntax-case stx ()
+    [(_ e clauses ...)
+     (with-syntax ([line (syntax-line stx)])
+       #'(begin
+           (printf "~a ~a\n" '(match e) line)
+           (rkt:match e clauses ...)))]))
+
 (struct ς-entr (σ f vs) #:transparent)
-(struct ς-exit (σ v) #:transparent)
-(struct ς-call (σ ρ f es k ℓ) #:transparent)
-(struct ς-tail (σ ρ f es ℓ) #:transparent)
+(struct ς-exit (σ v ω) #:transparent)
+(struct ς-call (σ ρ Ω f es ω k ℓ) #:transparent)
+(struct ς-tail (σ ρ Ω f es ω ℓ) #:transparent)
 
 (define empty-σ (hasheq))
 
@@ -37,31 +48,46 @@
       [(sref x)
        (hash-ref ρ x)])))
 
+(define (D e Ω)
+  (if (or (ulam? e)
+          (num? e))
+    (set (list))
+    (match e
+      [(href x)
+       (set 'heap-ref-what-to-do)]
+      [(sref x)
+       (hash-ref Ω x)])))
+
 (define (inject p)
   (ς-entr empty-σ p null))
 
-(define (ς-eval σ ρ e)
+(define ((union s1) s0)
+  (set-union s0 s1))
+
+(define (ς-eval σ ρ Ω e ω)
   (match e
     [(uapp f es k ℓ)
      (cond
        [(klam? k)
-	(list (ς-call σ ρ f es k ℓ))]
+	(list (ς-call σ ρ Ω f es ω k ℓ))]
        [(kref? k)
-	(list (ς-tail σ ρ f es ℓ))]
+	(list (ς-tail σ ρ Ω f es ω ℓ))]
        [else
 	(error 'ς-eval "unexpected k: ~a" k)])]
     [(kapp k e)
-     (let ([v (A e ρ σ)])
+     (let ([v (A e ρ σ)]
+           [ω (D e Ω)])
        (match k
 	 [(klam x e)
-	  (ς-eval σ (ρ-update x v ρ) e)]
+	  (ς-eval σ (ρ-update x v ρ) (hash-set Ω x ω) e (set))]
 	 [(kref k)
-	  (list (ς-exit σ v))]))]
+	  (list (ς-exit σ v ω))]))]
     [(if0 t c a)
-     (let ([v (A t ρ σ)])
+     (let ([v (A t ρ σ)]
+           [ωv (D t Ω)])
        (let* ([ςs null]
-	      [ςs (if (or (set-member? v 0) (set-member? v 'num)) (append (ς-eval σ ρ c) ςs) ςs)]
-	      [ςs (if (or (set-member? v 1) (set-member? v 'num)) (append (ς-eval σ ρ a) ςs) ςs)])
+	      [ςs (if (or (set-member? v 0) (set-member? v 'num)) (append (ς-eval σ ρ Ω c (set-union ω ωv)) ςs) ςs)]
+	      [ςs (if (or (set-member? v 1) (set-member? v 'num)) (append (ς-eval σ ρ Ω a (set-union ω ωv)) ςs) ςs)])
 	 ςs))]))
 
 (define succs
@@ -71,12 +97,15 @@
     [(ς-entr σ 'add1 (list v))
      (list (ς-exit σ (set 'num)))]
     [(ς-entr σ (ulam xs k e) vs)
-     (let ([ρ empty-ρ])
+     (let ([ρ empty-ρ]
+           [Ω (for/hasheq ([x (in-list xs)]
+                           [i (in-naturals)])
+                (values (var-x x) `(param ,i)))])
        (let ([σ (σ-update* xs vs σ)]
 	     [ρ (ρ-update* xs vs ρ)])
-	 (ς-eval σ ρ e)))]
-    [(or (ς-call σ ρ f es _ _)
-	 (ς-tail σ ρ f es _))
+	 (ς-eval σ ρ Ω e (set))))]
+    [(or (ς-call σ ρ Ω f es ω _ _)
+	 (ς-tail σ ρ Ω f es ω _))
      (let ([vs (map (λ (e) (A e ρ σ)) es)])
        (match f
 	 [(href 'add1)
@@ -96,14 +125,19 @@
     (foldl (λ (ς1 work) (propagate seen work ς0 ς1)) work ς1s))
 
   (define (update seen work ς0 ς1 ς2 ς3)
-    (match-let ([(ς-call σ1 ρ1 f1 es1 (klam x e) ℓ1) ς1]
+    (match-let ([(ς-call σ1 ρ1 Ω1 f1 es1 ω1 (klam x e) ℓ1) ς1]
 		[(ς-entr σ2 f2 vs2) ς2]
-		[(ς-exit σ3 v3) ς3])
+		[(ς-exit σ3 v3 ω3) ς3])
+      #;(displayln Ω1)
+      #;(displayln ω3)
       (let ([σ σ3]
-	    [ρ ρ1])
-	(let* ([ρ (if (href? f1) (ρ-update (hvar (ref-x f1)) (set f2) ρ) ρ)]
-	       [ρ (ρ-update x v3 ρ)])
-	  (propagate* seen work ς0 (ς-eval σ ρ e))))))
+	    [ρ ρ1]
+            [Ω Ω1])
+        ; fake-rebinding solution
+        (let ([ρ (if (href? f1) (ρ-update (hvar (ref-x f1)) (set f2) ρ) ρ)])
+          (let ([ρ (ρ-update x v3 ρ)]
+                [Ω (hash-set Ω x ω3)]) ; XXX TODO include the influence of the operator
+            (propagate* seen work ς0 (ς-eval σ ρ Ω e ω1)))))))
   
   (define (call seen work calls summaries ς0×ς1 ς2s f)
     (for/fold ([work work]
