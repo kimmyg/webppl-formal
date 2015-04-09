0439c9d890a9cba90b985f120df1f408f8c9e462
Kimball Germane <krgermane@gmail.com>
2015-04-08 22:43:49 -0600

    Concrete dependence

--- full/concrete.rkt
+++ full/concrete.rkt
@@ -4,24 +4,19 @@
 (define (flip p) (if (< (random) p) 0 1))
 (define (beta) (random))
 
-(struct clos (λ ρ) #:prefab)
-
-(struct kont () #:prefab)
-(struct halt kont () #:prefab)
-(struct fram kont (e ρ κ) #:prefab)
+(struct clos (λ ρ Ω) #:prefab)
 
 (define (update x v ρ)
   (hash-set ρ (var-x x) v))
+
+(define (update* xs vs ρ)
+  (foldl update ρ xs vs))
 
 (define (A e ρ)
   (if (ulam? e)
     (clos e ρ)
     (match e
-      [(fix f λ)
-       (let* ([ph (make-placeholder #f)]
-              [g (clos λ (update f ph ρ))])
-         (placeholder-set! ph g)
-         (make-reader-graph g))]
+
       [(num n)
        n]
       [(href x)
@@ -32,49 +27,56 @@
       [(sref x)
        (hash-ref ρ x)])))
 
-(define (D e Ω)
-  (match e))
+(struct branch (v ωv ω) #:transparent)
+(struct constant () #:transparent)
+(struct deterministic (f ωs) #:transparent)
 
-(define (K κ0 ρ κ)
+(define (eval* es ρ Ω)
+  (match es
+    [(list) (values (list) (list))]
+    [(cons e es)
+     (let-values ([(v ω) (eval e ρ Ω)]
+                  [(vs ωs) (eval* es ρ Ω)])
+       (values (cons v vs) (cons ω ωs)))]))
+
+(define (subst ω0 ω1)
+  (match ω0
+    [(constant) ω1]))
+
+(define (eval e ρ Ω)
   (cond
-    [(kref? κ0) κ]
-    [(klam? κ0) (fram κ0 ρ κ)]
+    [(lam? e)
+     (values (clos e ρ Ω) (constant))]
     [else
-     (error 'K "not for ~a" κ0)]))
-
-(define see
-  (match-lambda
-    [(halt) null]
-    [(fram κ0 ρ κ)
-     (cons (unP κ0) (see κ))]))
-
-(define (eval e ρ Ω H κ)
-  (displayln (see κ))
-  (match e
-    [(if0 t c a)
-     (let ([v (zero? (A t ρ))]
-           [ω (zero* (D t Ω))])
-       (if v
-         (eval c ρ Ω (cons (branch #t ω)) κ)
-         (eval a ρ Ω (cons (branch #f ω)) κ)))]
-    [(kapp κ0 e)
-     (kappl (K κ0 ρ κ) (A e ρ))]
-    [(uapp f es κ0 ℓ)
-     (uappl (A f ρ) (map (λ (e) (A e ρ)) es) (K κ0 ρ κ))]))
-
-(define (uappl f vs κ)
-  (if (procedure? f)
-    (kappl κ (apply f vs))
-    (match-let ([(clos (ulam xs k e) ρ) f])
-      (eval e (foldl update ρ xs vs) κ))))
-
-(define (kappl κ v)
-  (match κ
-    [(halt) v]
-    [(fram (klam x e) ρ Ω H κ)
-     (eval e (update x v ρ) Ω H κ)]))
+     (match e
+       [(app f es ℓ)
+        (let-values ([(vf ωf) (eval f ρ Ω)]
+                     [(vs ωs) (eval* es ρ Ω)])
+          (if (procedure? vf)
+            (values (apply vf vs) (subst ωf (deterministic vf ωs)))
+            (match-let ([(clos (lam xs e) ρ Ω) vf])
+              (eval e (update* xs vs ρ) (update* xs ωs Ω)))))]
+       [(fix f λ)
+        (let* ([ph (make-placeholder #f)]
+               [g (clos λ (update f ph ρ) (update f (constant) Ω))])
+         (placeholder-set! ph g)
+         (values (make-reader-graph g) (constant)))]
+       [(href x)
+        (values (case x
+                  [(*) *]
+                  [(sub1) sub1]
+                  [else (error 'eval "no primitive found: ~a" x)])
+                (constant))]
+       [(if0 et ec ea)
+        (let*-values ([(v0 ω0) (eval et ρ Ω)]
+                      [(v1 ω1) (eval (if (zero? v0) ec ea) ρ Ω)])
+          (values v1 (branch (zero? v0) ω0 ω1)))]
+       [(num n)
+        (values n (constant))]
+       [(sref x)
+        (values (hash-ref ρ x) (hash-ref Ω x))])]))
 
 (define (inject p)
-  (uappl (A p (hasheq)) null (halt)))
+  (eval p (hasheq) (hasheq)))
 
-(inject (CPS (P ((λ (fact) (fact 5)) (fix fact (λ (n) (if0 n 1 (* n (fact (sub1 n))))))))))
+(inject (P ((λ (fact) (fact 5)) (fix fact (λ (n) (if0 n 1 (* n (fact (sub1 n)))))))))
