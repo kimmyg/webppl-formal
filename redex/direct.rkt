c7f2a99672b2120db1e56912e719e7aec1866f11
Kimball Germane <krgermane@gmail.com>
2015-04-17 23:02:33 -0600

    Oracles!

--- redex/direct.rkt
+++ redex/direct.rkt
@@ -28,7 +28,45 @@
      (rat-κ ρ (e ...) ℓ ι κ)
      (ran-κ v (v ...) ρ (e ...) ℓ ι κ)])
 
-(define ->
+(define (->meta f)
+  (case f [(+) +] [(-) -] [(*) *] [(/) /] [else (error '->meta "not defined for ~a" f)]))
+
+(define-metafunction LM
+  lookup : ρ x -> v
+  [(lookup ((x_0 v_0) (x v) ...) x_0) v_0]
+  [(lookup ((x_1 v_1) (x v) ...) x_0) (lookup ((x v) ...) x_0)])
+
+(define-metafunction LM
+  extend : ρ x v -> ρ
+  [(extend ρ x v)
+   ((x v) . ρ)])
+
+(define-metafunction LM
+  extend* : ρ (x ...) (v ...) -> ρ
+  [(extend* ρ () ()) ρ]
+  [(extend* ρ (x_0 x ...) (v_0 v ...))
+   (extend* (extend ρ x_0 v_0) (x ...) (v ...))])
+
+(require math/distributions)
+
+(define (backflip x p) (flbernoulli-inv-cdf p x #f #f))
+(define (backbeta x α β) (flbeta-inv-cdf α β x #f #f))
+(define (backgaussian x μ σ²) (flnormal-inv-cdf μ (sqrt σ²) x #f #f))
+
+(define (make-> O)
+  (define-metafunction LM
+    apply : v (v ...) ι κ -> ς
+    [(apply prim (n_0 n_1) ι κ)
+     (ap κ ,((->meta (term prim)) (term n_0) (term n_1)))]
+    [(apply flip (v_p) ι κ)
+     (ap κ ,(backflip (O (term ι)) (term v_p)))]
+    [(apply beta (v_α v_β) ι κ)
+     (ap κ ,(backbeta (O (term ι)) (term v_α) (term v_β)))]
+    [(apply beta (v_μ v_σ²) ι κ)
+     (ap κ ,(backgaussian (O (term ι)) (term v_μ) (term v_σ²)))]
+    [(apply (clos (λ (x ..._0) e) ρ) (v ..._0) ι κ)
+     (ev e (extend* ρ (x ...) (v ...)) ι κ)])
+
   (reduction-relation
    LM
    #:domain ς
@@ -69,32 +107,16 @@
    [--> (ap (let-κ x ρ_1 e ι κ) v)
         (ev e (extend ρ_1 x v) ι κ)]))
 
-(define-metafunction LM
-  lookup : ρ x -> v
-  [(lookup ((x_0 v_0) (x v) ...) x_0) v_0]
-  [(lookup ((x_1 v_1) (x v) ...) x_0) (lookup ((x v) ...) x_0)])
+(define -> (make-> (λ (ι) 0.5)))
 
-(define-metafunction LM
-  extend : ρ x v -> ρ
-  [(extend ρ x v)
-   ((x v) . ρ)])
 
-(define-metafunction LM
-  extend* : ρ (x ...) (v ...) -> ρ
-  [(extend* ρ () ()) ρ]
-  [(extend* ρ (x_0 x ...) (v_0 v ...))
-   (extend* (extend ρ x_0 v_0) (x ...) (v ...))])
 
-(define-metafunction LM
-  apply : v (v ...) ι κ -> ς
-  [(apply prim (n ...) ι κ)
-   (ap κ 42)]
-  [(apply (clos (λ (x ..._0) e) ρ) (v ..._0) ι κ)
-   (ev e (extend* ρ (x ...) (v ...)) ι κ)])
 
 #;(traces -> (term (ev (let ([x 5]) x) () halt-κ)))
-#;(apply-reduction-relation* -> (term (ev (+ 2 2) () halt-κ)))
-(traces -> (term (ev ((λ (x) (x x ℓ1)) (λ (y) (y y ℓ2)) ℓ0) () () halt-κ)))
+(apply-reduction-relation* -> (term (ev (flip 0.5 ℓ0) () () halt-κ)))
+#;(traces -> (term (ev ((λ (x) (x x ℓ1)) (λ (y) (y y ℓ2)) ℓ0) () () halt-κ)))
+
+
 
 #;(render-reduction-relation -> "direct-concrete.ps")
 
