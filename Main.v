2c82323983221fa5b14c9c4b4368874384bb840f
Kimball Germane <krgermane@gmail.com>
2015-04-24 14:48:57 -0600

    Reset

--- Main.v
+++
@@ -1,4 +0,0 @@
-(* We want to formally relate the semantics of WebPPL to Markov dependencies so that we can relate portions of execution (and perhaps program fragments) to Markov networks. *)
-
-Require Import List.
-
